e9a52916330e64a27e87315d264cf92043350df3
Justin Merz <jrmerz@gmail.com>
2017-06-13 16:41:12 -0700

    adding batch uploading

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ var request = require('superagent').agent();
 var usernamePass = require('./username-password');
 
 module.exports = {
-  host : 'http://a.dams-sandbox.casil.ucdavis.edu',
+  protocol : 'http://',
+  host : 'a.dams-sandbox.casil.ucdavis.edu',
   // host : 'http://localhost:3000',
   username : usernamePass.username,
   password : usernamePass.password,
--- test.js
+++ test.js
@@ -3,32 +3,45 @@ const login = require('./lib/login');
 const config = require('./config');
 const addWork = require('./lib/addWork');
 
+var count = 0;
+var batchSize = 10;
+
 async function addWorks(){
   var jsonStreamParser = require('./lib/jsonStreamParser');
 
   await jsonStreamParser(
-    './data/a:0100.json.stream', 
-    async (entry, next) => {
-      try {
-        var files = [path.join(__dirname, 'data/cat.jpg')];
-        console.log(files);
+    './data/a:0100.json.stream', batchSize,
+    async (entries, next) => {
 
-        console.time('Total Work Time '+entry.identifier);
-        // entry.admin_set_id = 'ef288949-2bee-4f3e-855c-5ccb48fe7c74';
-        entry.admin_set_id = '6294e459-2909-49de-9039-0937492d00b8';
-        await addWork(entry, files);
+      console.time('Batch time');
 
-        console.log(`Added: ${entry.identifier}`);
-        console.timeEnd('Total Work Time '+entry.identifier);
-        console.log('');
+      var files = [path.join(__dirname, 'data/cat.jpg')];
 
-        next();
-      } catch(e) {
-        throw e;
-      }
+      console.log('Adding Entries '+count+' to '+(count+entries.length));
+
+      await Promise
+        .all(entries.map(async entry => await addEntry(entry, files)))
+        .then(() => {
+          console.timeEnd('Batch time');
+          console.log();
+          count += entries.length;
+          next();
+        });
+
       
+
     }
   );
+}
+
+async function addEntry(entry, files) {
+  console.time('Total Work Time '+entry.identifier);
+  // entry.admin_set_id = 'ef288949-2bee-4f3e-855c-5ccb48fe7c74';
+  entry.admin_set_id = '6294e459-2909-49de-9039-0937492d00b8';
+  await addWork(entry, files);
+
+  console.timeEnd('Total Work Time '+entry.identifier);
+
 }
 
 (async function(){
